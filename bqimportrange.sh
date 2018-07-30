dcd725c57216159d867598a2384024a371ab7864
Lev <lev-savranskiy@users.noreply.github.com>
2018-07-30 01:28:24 -0400

    Update bqimportrange.sh

--- bqimportrange.sh
+++ bqimportrange.sh
@@ -22,7 +22,7 @@ date "+%m-%d-%Y %H:%M:%S" >> ${logpath}
 #table name cant contain - and .
 TABLE_NAME=${FILE//[-.]/_}
 
-#if [ ${DATASET} = "bridge_dataset_temp" ]
+#if [ ${DATASET} = "test" ]
 #then
 #TABLE_NAME=${TABLE_NAME//[.csv]/}
 #fi
@@ -33,7 +33,7 @@ echo "${DATASET}.${TABLE_NAME}"
 bq mk --table --description table_from_CLI eminent-torch-384:${DATASET}.${TABLE_NAME} ${SCHEMA} >> ${logpath}
 
 
-if [ ${DATASET} = "bridge_dataset_temp" ]
+if [ ${DATASET} = "test" ]
 then
     #master is pipe delimited, with header
     bq load -F "|" --replace --source_format=CSV --max_bad_records=100000 --skip_leading_rows=1 ${DATASET}.${TABLE_NAME} ${FOLDER}/${FILE} &
